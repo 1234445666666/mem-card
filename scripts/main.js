f3e522c09d4a336643caa18611b997a1627680a6
1234445666666 <p.k.270607@gmail.com>
2025-02-10 13:43:52 +0300

    creating a design

--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,42 @@
 //добавление картинки
-const inputImg = document.querySelector('#input__img');
-const inputText = document.querySelector('#input__text');
-const addBtn = document.querySelector('#add__to__do');
+const inputImg = document.querySelector("#input__img");
+const inputText = document.querySelector("#input__text");
+const addBtn = document.querySelector("#add__to__do");
 
-addBtn.addEventListener('click', () => {
-    if (inputImg.value.trim() !== '' && inputText.value.trim() !== '') {
-        createCard(inputImg.value, inputText.value);
+addBtn.addEventListener("click", () => {
+  if (inputImg.value.trim() !== "" && inputText.value.trim() !== "") {
+    createCard(inputImg.value, inputText.value);
 
-        inputImg.value = '';
-        inputText.value = '';
-    } else {
-        alert('Введите задачу');
-    }
+    inputImg.value = "";
+    inputText.value = "";
+  } else {
+    alert("Введите задачу");
+  }
 });
 
 const createCard = (img, text) => {
-    let todoMem = document.createElement('div');
-    todoMem.className = 'to__do__mem';
-    todoMem.insertAdjacentHTML('beforeend', `
+  let todoMem = document.createElement("div");
+  todoMem.className = "to__do__mem";
+  todoMem.insertAdjacentHTML(
+    "beforeend",
+    `
         <span class="icon like__card"></span>
         <img class="img__card" src="${img}"></img>
         <span class="text__card">${text}</span>
         <span class="icon delete__card"></span>
-    `);
+    `
+  );
 
-    document.body.appendChild(todoMem);
+  document.body.appendChild(todoMem);
 
-    let likeCard = todoMem.querySelector('.like__card');1
-    let deleteCard = todoMem.querySelector('.delete__card');
+  let likeCard = todoMem.querySelector(".like__card");
+  let deleteCard = todoMem.querySelector(".delete__card");
 
-    likeCard.addEventListener('click', () => {
-        likeCard.classList.toggle('liked');
-    });
+  likeCard.addEventListener("click", () => {
+    likeCard.classList.toggle("liked");
+  });
 
-    deleteCard.addEventListener('click', () => {
-        todoMem.remove();
-    });
+  deleteCard.addEventListener("click", () => {
+    todoMem.remove();
+  });
 };
